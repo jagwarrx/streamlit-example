e78d109a5c98e66a10526f4f5000251460c63194
Hari Meyyappan <hari.m.subbiah@gmail.com>
2023-06-14 15:01:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,59 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from PyPDF2 import PdfReader
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.prompts import PromptTemplate
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+pdf_files = st.file_uploader("Upload pdf files", type=["pdf"],
+                               accept_multiple_files=False)
+if pdf_files is not None:
+  pdf_reader = PdfReader(pdf_files)
+  text = ""
+  for page in pdf_reader.pages:
+    text += page.extract_text()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+OPENAI_API_KEY = st.text_input("API Key:")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# split into chunks
+text_splitter = CharacterTextSplitter(
+  separator="\n",
+  chunk_size=1000,
+  chunk_overlap=200,
+  length_function=len
+)
+chunks = text_splitter.split_text(text)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+prompt_template = """
 
+One sided agreements are where the Supplier shall bear full responsibility for indemnifying the Customer against any losses or damages caused by the Supplier's unauthorized use, disclosure, or misappropriation of the Customer's confidential information. 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+Two sided agreements are where the Customer and Supplier mutually agree to indemnify and defend each other against any claims, damages, liabilities, losses, costs, and expenses arising out of third-party intellectual property infringement claims related to their respective products, services, or deliverables provided under this Agreement.
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+{context}
+Question: {question}
+Answer:"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context","question"]
+)
 
-    points_per_turn = total_points / num_turns
+user_question = "Is this agreement one-sided indemnification or two-sided indemnification? Provide instances of example caluses."
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# create embeddings
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# show user input
+if user_question:
+  docs = knowledge_base.similarity_search(user_question)
+
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+chain = load_qa_chain(llm, chain_type="stuff", prompt=PROMPT)
+with get_openai_callback() as cb:
+  response = chain.run(input_documents=docs, question=user_question)
+  st.write(response)
+  
