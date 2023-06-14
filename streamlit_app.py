6c6aee2eee413d3a12f1b6de4e782ff73b9b92d3
Hari Meyyappan <hari.m.subbiah@gmail.com>
2023-06-14 15:05:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
-!pip install langchain
-!pip install openai
-!pip install pypdf2
-!pip install faiss-cpu
-!pip install tiktoken
-!pip install -q streamlit
+
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
